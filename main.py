d1c8335b7396a5ff7341eeb9c645e280e82c3d30
sanjaya <62214882+Sanjaya-Danushka@users.noreply.github.com>
2025-06-11 13:14:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
 print ("hello world")
+def generate_subsets(nums, index=0, current_subset=None):
+    if current_subset is None:
+        current_subset = []
+        
+    if index == len(nums):
+        print(current_subset)
+        return
+    
+    generate_subsets(nums, index + 1, current_subset + [nums[index]])
+    generate_subsets(nums, index + 1, current_subset)
+
+numbers = [1, 2, 3]
+generate_subsets(numbers)
